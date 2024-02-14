78a7fa698ddfa7da8b27e604a2062b7842a42a5e
Tasweeb Ammad <129410196+Tasweeb-Ammad@users.noreply.github.com>
2024-02-14 13:20:51 +0500

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCz0VjZsv5yysEGoeJPzrnE3JJPT-7TXLo",
-  authDomain: "todo-app-c0004.firebaseapp.com",
-  projectId: "todo-app-c0004",
-  storageBucket: "todo-app-c0004.appspot.com",
-  messagingSenderId: "961796772826",
-  appId: "1:961796772826:web:323822a142d5b1ac6aa6e3",
+  //add firebase config here
 };
 
 const app = initializeApp(firebaseConfig);
